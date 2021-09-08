cecab7926e3a076adac8beadc736d26e7e73eaa2
assilbouassida <bouassidaassil40@gmail.com>
2021-09-08 14:05:01 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,89 @@
+export const moviedata = [
+    
+    {
+        id:Math.random(),
+      title: "The good doctor",
+      posterUrl: "https://pbs.twimg.com/media/EbNkkiTXsAc2o3Y.jpg",
+      rate: 4.2,
+      description:
+        "Shaun Murphy, a young surgeon with autism and Savant syndrome, is recruited into the surgical unit of a prestigious hospital.",
+      trailer: "https://www.youtube.com/embed/msJggy8xtmI",
+    },
+    
+    {
+      id:Math.random(),
+      title: "Prison Break",
+      posterUrl:
+        "https://upload.wikimedia.org/wikipedia/en/5/54/Prison-break-season-4-dvd.jpg",
+      rate: 4.2,
+      description:
+        "Due to a political conspiracy, an innocent man is sent to death row and his only hope is his brother, who makes it his mission to deliberately get himself sent to the same prison in order to break the both of them out, from the inside.",
+      trailer: "https://www.youtube.com/embed/AL9zLctDJaU",
+    },
+    
+    {
+      id:Math.random(),
+      title: "Taboo",
+      posterUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/81BbPS82fnL._AC_SL1500_.jpg",
+      rate: 4,
+      description:
+        "Adventurer James Keziah Delaney returns to London during the War of 1812 to rebuild his late father's shipping empire. However, both the government and his biggest competitor want his inheritance at any cost - even murder.",
+      trailer: "https://www.youtube.com/embed/W1fiijqrKuc",
+    },
+   
+    {
+      id:Math.random(),
+      title: "Dexter",
+      posterUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/81JKXT1j0OL._SL1500_.jpg",
+      rate: 4.2,
+      description:
+        "By day, mild-mannered Dexter is a blood-spatter analyst for the Miami police. But at night, he is a serial killer who only targets other murderers.",
+      trailer: "https://www.youtube.com/embed/YQeUmSD1c3g",
+    },
+   
+    {
+      id:Math.random(),
+      title: "Black List",
+      posterUrl:
+        "https://i2.wp.com/www.nerdsandbeyond.com/wp-content/uploads/2018/11/203AD589-D9BC-4842-BE7A-3FAC786F49EA.jpeg?fit=750%2C947&ssl=1",
+      rate: 3.9,
+      description:
+        "A new FBI profiler, Elizabeth Keen, has her entire life uprooted when a mysterious criminal, Raymond Reddington, who has eluded capture for decades, turns himself in and insists on speaking only to her.",
+      trailer: "https://www.youtube.com/embed/XihA6GWIBdM",
+    },
+    
+    {
+      id:Math.random(),
+      title: "Vis A Vis",
+      posterUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR1DBIEAneCfzyHa1-xYPa2oMCulo6mken_9Q&usqp=CAU",
+      rate: 3.8,
+      description:
+        "Locked Up, originally titled in Spanish as Vis A Vis , is a Spanish serial drama television series produced by Globomedia, initially for Spanish Network Antena 3 and later on for Fox Spain. It premiered on 20 April 2015.[2] The series begins by focusing on a young woman who is sent to prison and then goes on to depict the image of prison and law enforcement systems.",
+      trailer: "https://www.youtube.com/embed/ceNKwOvSVm8",
+    },
+    {
+      id:Math.random(),
+      title: "Cukur",
+      posterUrl:
+        "https://imgr.cineserie.com/2018/10/1129749.jpg?imgeng=/f_jpg/cmpr_0/w_744/h_1200/m_cropbox&ver=1",
+      rate: 4,
+      description:
+        "A dangerous neighborhood, The Pit, ran by a noble mafia family called Koçovars. When the family is in the danger of losing the control of The Pit, their youngest son now must come back to his home, where he could never escape from.",
+      trailer: "https://www.youtube.com/embed/Ix43lQOjLGw",
+    },
+   
+    {
+      id:Math.random(),
+      title: "SHERLOCK",
+      posterUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/51j90UOkX-L.jpg",
+      rate: 4.6,
+      description:
+        "A modern update finds the famous sleuth and his doctor partner solving crime in 21st century London.",
+      trailer: "https://www.youtube.com/embed/IrBKwzL3K7s",
+    },
+  ];
+  
